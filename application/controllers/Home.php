0410a5a3f822399bf151150f805690fd43147c4a
aryapradata <aryapradata@gmail.com>
2020-03-13 23:31:39 +0700

    login update v2

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,10 +4,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller{
 
+    public function __construct()
+    {
+        parent::__construct();
+		// Load database
+		$this->load->model('homepage_database');
+	}
+
     public function index(){
         //Controller Home
         $this->load->view('layout/header');
-        $this->load->view('home');
+        $data['blog'] = $this->homepage_database->getBlogs();
+		$data['portofolio'] = $this->homepage_database->getPortofolios();
+		$data['tentang'] = $this->homepage_database->getTentang();
+		$data['kelas'] = $this->homepage_database->getKelas();
+        $this->load->view('home', $data);
         $this->load->view('layout/footer');
     }
     
