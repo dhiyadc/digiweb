0c121499e2a76959b7c7c4063b803763c985d79d
aryapradata <aryapradata@gmail.com>
2020-03-14 16:13:38 +0700

    fix

--- application/views/_partials/header_admin.php
+++ application/views/_partials/header_admin.php
@@ -47,7 +47,7 @@
       <!--logo end-->
       <div class="top-menu">
         <ul class="nav pull-right top-menu">
-          <li><a class="logout" href="login.html">Logout</a></li>
+          <li><a class="logout" href="<?= base_url() ?>admin/logout">Logout</a></li>
         </ul>
       </div>
     </header>
