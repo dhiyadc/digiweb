e5bf02c6904111e3f7d3db5641d014612040c6f7
mtiansyahp <m.tiansyahp@yahoo.com>
2020-03-15 21:31:27 +0700

    modal daftar les

--- application/views/home.php
+++ application/views/home.php
@@ -1,4 +1,25 @@
+<!doctype html>
+<html lang="en">
 
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="<?php echo base_url(); ?>assets/img/favicon.png">
+	<title>Digital Creative</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css"> 
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendors/linericon/style.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendors/owl-carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendors/nice-select/css/nice-select.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendors/animate-css/animate.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendors/flaticon/flaticon.css">
+    <!-- main css -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+</head>
 
 <body>
 	
@@ -523,3 +544,21 @@
 	<!--================ End Newsletter Area ================-->
 	
 </body>
+		<!-- JS -->
+		<script src="<?php echo base_url(); ?>assets/js/jquery-3.2.1.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/popper.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/stellar.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/jquery.magnific-popup.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/vendors/isotope/imagesloaded.pkgd.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/vendors/isotope/isotope-min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/vendors/owl-carousel/owl.carousel.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/jquery.ajaxchimp.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/vendors/counter-up/jquery.waypoints.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/vendors/counter-up/jquery.counterup.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/mail-script.js"></script>
+		<!--gmaps Js-->
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+		<script src="<?php echo base_url(); ?>assets/js/gmaps.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/theme.js"></script>
--- application/views/page/pricing.php
+++ application/views/page/pricing.php
@@ -30,7 +30,7 @@
 				</div>
 				<div class="row">
 					<div class="col-lg-4 col-md-6">
-						<div class="pricing_item">
+						<div class="pricing_item active">
 							<h3 class="p_title">Android Package</h3>
 							<h1 class="p_price">Rp.900.000</h1>
 							<div class="p_list">
@@ -41,7 +41,7 @@
 								</ul>
 							</div>
 							<div class="p_btn">
-								<a class="gradient_btn" href="#"><span>Join Now</span></a>
+								<button type="button" data-toggle="modal" data-target="#contact-modal"><a class="gradient_btn"><span>Join Us</span></a></button>
 							</div>
 						</div>
 					</div>
@@ -57,12 +57,12 @@
 								</ul>
 							</div>
 							<div class="p_btn">
-								<a class="gradient_btn" href="#"><span>Join Now</span></a>
+								<button type="button" data-toggle="modal" data-target="#contact-modal"><a class="gradient_btn"><span>Join Us</span></a></button>
 							</div>
 						</div>
 					</div>
 					<div class="col-lg-4 hidden-md">
-						<div class="pricing_item">
+						<div class="pricing_item active">
 							<h3 class="p_title">Basic Package</h3>
 							<h1 class="p_price">Rp.350.000</h1>
 							<div class="p_list">
@@ -73,7 +73,7 @@
 								</ul>
 							</div>
 							<div class="p_btn">
-								<a class="gradient_btn" href="#"><span>Join Now</span></a>
+								<button type="button" data-toggle="modal" data-target="#contact-modal"><a class="gradient_btn"><span>Join Us</span></a></button>
 							</div>
 						</div>
 					</div>
@@ -83,6 +83,77 @@
 	<section>
 		<!-- Modal Daftar Les -->
 		
+			<div id="contact-modal" class="modal fade bd-example-modal-lg" role="dialog">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<a class="close" data-dismiss="modal">×</a>
+							<h3>Form Les Android</h3>
+						</div>
+						<form id="contactForm" name="contact" role="form">
+							<div class="modal-body">				
+								<div class="form-group">
+									<label for="name">Nama</label>
+									<input type="text" name="name" class="form-control" required>
+								</div>
+								<div class="form-group">
+									<label for="tel">No Telpon</label>
+									<input type="tel" name="phone" class="form-control" required>
+								</div>
+								<div class="form-group">
+									<label for="message">Alamat</label>
+									<textarea name="message" class="form-control" required></textarea>
+								</div>
+								<div class="form-group">
+									<label for="email">Email</label>
+									<input type="email" name="email" class="form-control" required>
+								</div>	
+								<!-- JS -->
+								
+								<!--  -->
+								<div class="form-group">
+								<div class="row">
+									<div class="col-md-12">
+										<script>
+											// Material Select Initialization
+											$(document).ready(function() {
+											$('.mdb-select').materialSelect();
+											});
+										</script>
+										<select class="mdb-select colorful-select dropdown-primary md-form" multiple searchable="Search here..">
+										<option value="" disabled selected>Pilih Paketmu</option>
+										<option value="1">Android</option>
+										<option value="2">Basic</option>
+										<option value="3">Website</option>
+										</select>
+
+									</div>
+									</div>
+								</div>
+									
+							</div>
+							<div class="modal-footer">					
+								<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+								<button type="submit" class="btn btn-success" id="submit">Confirm</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		<!--  -->
+		<script>
+      function konfirmasi(){
+			var tanya = confirm("Apakah Anda  Data Ini Sudah Benar?");
+	
+			if(tanya === true) {
+				pesan = "Ya";
+			}else{
+				pesan = "Tidak";
+			}
+	
+			document.getElementById("pesan").innerHTML = pesan;
+		}
+    	</script>
 		<!--  -->
 	</section>
 </body>
