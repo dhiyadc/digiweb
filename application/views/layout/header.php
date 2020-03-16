135368e56c5797c25e1d57c457f8420ac6096533
dhiyadc <dhiyacalista@gmail.com>
2020-03-16 15:35:30 +0700

    change header

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -41,11 +41,8 @@
 						<ul class="nav navbar-nav menu_nav justify-content-center">
 							<li class="nav-item active"><a class="nav-link" href="<?php echo base_url(); ?>">Home</a></li>
 							<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>tentang_user/viewTentang">About</a></li>
-							<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>portofolio_user">Portofolio </a>
-							<li class="nav-item submenu dropdown">
-								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-								 aria-expanded="false">Layanan Kami</a>
-							</li>
+							<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>portofolio_user">Portofolio </a></li>
+							<li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>kelas">Layanan Kami</a></li>
 							<li class="nav-item submenu dropdown">
 								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
 								 aria-expanded="false">Blog</a>
