1a2b8fc3ebefbbe1396801abd8696e7c4c772f8b
dhiyadc <dhiyacalista@gmail.com>
2020-03-19 17:31:46 +0700

    adding letter spacing on title

--- application/views/about-us.php
+++ application/views/about-us.php
@@ -22,7 +22,7 @@
 			<div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
 			<div class="container">
 				<div class="banner_content text-center">
-					<h2 style="color: white; text-shadow: 2px 2px 4px #000000;">About Us</h2>
+					<h2 style="color: white; text-shadow: 2px 2px 4px #000000; letter-spacing: 10px; word-spacing: 10px;">About Us</h2>
 				</div>
             </div>
         </div>
--- application/views/kelas/index.php
+++ application/views/kelas/index.php
@@ -7,11 +7,7 @@
 				<div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
 				<div class="container">
 					<div class="banner_content text-center">
-						<div class="page_link">
-							<a href="index.html">Home</a>
-							<a href="about-us.html">Layanan Kami</a>
-						</div>
-						<h2 style="color: white; text-shadow: 2px 2px 4px #000000;">Layanan Kami</h2>
+						<h2 style="color: white; text-shadow: 2px 2px 4px #000000; letter-spacing: 10px; word-spacing: 10px;">Layanan Kami</h2>
 					</div>
 				</div>
 			</div>
