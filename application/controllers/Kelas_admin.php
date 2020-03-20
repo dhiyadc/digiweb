11e8d24c70333a3301b9b6fa07dff81a46a025ec
zorayaw <zorayaw31@gmail.com>
2020-03-20 20:08:25 +0700

    page access, merge about us to tentang_user. toggle switch deleted

--- application/controllers/Kelas_admin.php
+++ application/controllers/Kelas_admin.php
@@ -1,6 +1,7 @@
 <?php
 
-class Kelas_admin extends CI_Controller {
+class Kelas_admin extends CI_Controller
+{
     public function __construct()
     {
         parent::__construct();
@@ -9,50 +10,79 @@ class Kelas_admin extends CI_Controller {
 
     public function index()
     {
-        $data['kelas'] = $this->Kelas_model->getAllClass();
-        $this->load->view('_partials/header_admin');
-        $this->load->view('kelas_admin/index',$data);
-        $this->load->view('_partials/footer_admin');
+        if ($this->session->userdata('logged_in')) {
+            $data['kelas'] = $this->Kelas_model->getAllClass();
+            $this->load->view('_partials/header_admin');
+            $this->load->view('kelas_admin/index', $data);
+            $this->load->view('_partials/footer_admin');
+        } else {
+            redirect('admin');
+        }
     }
 
     public function create_form()
     {
-        $data['kategori'] = $this->Kelas_model->getKategori();
-        $this->load->view('_partials/header_admin');
-        $this->load->view('kelas_admin/create_class',$data);
-        $this->load->view('_partials/footer_admin');
+        if ($this->session->userdata('logged_in')) {
+            $data['kategori'] = $this->Kelas_model->getKategori();
+            $this->load->view('_partials/header_admin');
+            $this->load->view('kelas_admin/create_class', $data);
+            $this->load->view('_partials/footer_admin');
+        } else {
+            redirect('admin');
+        }
     }
 
-    public function create() {   
-        $this->Kelas_model->createClass();
-        redirect('kelas_admin');   
+    public function create()
+    {
+        if ($this->session->userdata('logged_in')) {
+            $this->Kelas_model->createClass();
+            redirect('kelas_admin');
+        } else {
+            redirect('admin');
+        }
     }
 
     public function delete($id)
     {
-        $this->Kelas_model->deleteClass($id);
-        redirect('kelas_admin');
+        if ($this->session->userdata('logged_in')) {
+            $this->Kelas_model->deleteClass($id);
+            redirect('kelas_admin');
+        } else {
+            redirect('admin');
+        }
     }
 
     public function update_form($id)
     {
-        $data['kelas'] = $this->Kelas_model->getClassById($id);
-        $this->load->view('_partials/header_admin');
-        $this->load->view('kelas_admin/update_class',$data);
-        $this->load->view('_partials/footer_admin');
+        if ($this->session->userdata('logged_in')) {
+            $data['kelas'] = $this->Kelas_model->getClassById($id);
+            $this->load->view('_partials/header_admin');
+            $this->load->view('kelas_admin/update_class', $data);
+            $this->load->view('_partials/footer_admin');
+        } else {
+            redirect('admin');
+        }
     }
 
     public function update($id)
     {
-        $this->Kelas_model->updateClass($id);
-        redirect('kelas_admin/detail_class/' . $id);
+        if ($this->session->userdata('logged_in')) {
+            $this->Kelas_model->updateClass($id);
+            redirect('kelas_admin/detail_class/' . $id);
+        } else {
+            redirect('admin');
+        }
     }
 
     public function detail_class($id)
     {
-        $data['kelas'] = $this->Kelas_model->getClassById($id);
-        $this->load->view('_partials/header_admin');
-        $this->load->view('kelas_admin/detail_class',$data);
-        $this->load->view('_partials/footer_admin');
+        if ($this->session->userdata('logged_in')) {
+            $data['kelas'] = $this->Kelas_model->getClassById($id);
+            $this->load->view('_partials/header_admin');
+            $this->load->view('kelas_admin/detail_class', $data);
+            $this->load->view('_partials/footer_admin');
+        } else {
+            redirect('admin');
+        }
     }
 }
